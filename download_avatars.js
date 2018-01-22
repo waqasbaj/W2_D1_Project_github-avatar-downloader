0a33797e189e96aa6ffc18e9db1b493468b77f46
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-22 20:50:12 +0000

    Extracted all pictures from GIT

--- download_avatars.js
+++ download_avatars.js
@@ -19,6 +19,40 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   for (i=0; i<result.length; i++)
   {
     console.log("Result",  result[i].avatar_url);
+    downloadImageByURL(result[i].avatar_url, result[i].login);
   }
 
 });
+
+function downloadImageByURL(url, filePath ) {
+
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get(url)
+              // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {
+        console.log('Downloading image...');                          // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message: ', response.statusMessage);
+         console.log('Content Type: ', response.headers['content-type']);
+
+       })
+       .on('end', function() {
+        console.log('Download complete.');
+        })
+        .pipe(fs.createWriteStream('./'+filePath+'.jpg'))
+}
+              // Note 4
+
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "/avatars");
